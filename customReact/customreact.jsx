194b4cf9e0f690025abeacd56393ed91464c17ed
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-04-23 11:57:02 +0500

    password

--- customReact/customreact.jsx
+++ customReact/customreact.jsx
@@ -1,34 +1,26 @@
-// Custom function to render a React-like element into the DOM
-function customRender(reactElement, container) {
-  // Create a DOM element based on the 'type' specified in the reactElement
-  const domElement = document.createElement(reactElement.type);
+<script>
+    let num =[3,5,1,2,4]
+    // for(let i=0; i<num.length; i++){
+    //   console.log(num[i])
+    // }
 
-  // Set the inner text or HTML content of the element
-  // Note: There is a typo in 'children', which should be corrected
-  domElement.innerHTML = reactElement.childern; // <-- 'childern' should be 'children'
+    // forEach loop
+    num.forEach((element)=>{
+      console.log(element*element)
+    })
 
-  // Loop through the props object to set attributes on the created DOM element
-  for (const prop in reactElement.props) {
-    if (prop === 'children') continue; // Skip the 'children' prop if present
-    domElement.setAttribute(prop, reactElement.props[prop]); // Set other props as attributes
-  }
+    // Array.from
+    let name= "Waheed"
+    let arr=Array.from(name)
+    console.log(arr)
 
-  // Finally, append the newly created element to the specified container
-  container.appendChild(domElement); // <-- Missing in the original function
-}
+    //  foo .. of  *
+    for(let item of num){
+      console.log(item)
+    }
 
-// Define a React-like element with type, props, and children
-const reactElement = {
-  type: 'a',
-  props: {
-    href: 'https://google.com',
-    target: '_blank'
-  },
-  childern: 'Click me to visit google' // <-- Typo: should be 'children'
-}
-
-// Select the container element from the DOM
-const mainContainer = document.querySelector('#root');
-
-// Call the custom render function to render the element
-customRender(reactElement, mainContainer);
+    // for.. in
+    for(let i in num){
+      console.log(num[i])
+    }
+  </script>
