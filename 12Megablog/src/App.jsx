d8fc2a9d950cd44d354ba4ffe860a88021fd8642
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-06-16 20:53:35 +0500

    tailwind

--- 12Megablog/src/App.jsx
+++ 12Megablog/src/App.jsx
@@ -1,15 +1,39 @@
-
-
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'; // make sure this has a getCurrentUser() method
+import  {login, logout} from "./store/authSlice"
+import './App.css';
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
+  useEffect(() => {
+    // Check if user is logged in
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch({ type: "LOGIN", payload: userData }); // adjust action as per your setup
+        } else {
+          dispatch({ type: "LOGOUT" });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [dispatch]);
+
+  return !loading ? (
+    <div className='min-h'></div>
   )
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+ 
 }
 
-export default App
+export default App;
---
+++ 12Megablog/src/store/store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+// create a store
+const store=configureStore({
+    reducer:{
+
+    }
+});
+
+export default store;
