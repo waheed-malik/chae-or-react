5261ee4e6ee71ec3305eb5f8d1100997a931bffd
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-06-17 18:58:55 +0500

    Mega

--- 12Megablog/src/components/index.js
+++ 12Megablog/src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
-
+import Container from "./containe/Containerr";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
